41f1c5a4b15df4fba1a375d06f7d1e5017cadcd7
MoosaTae <sadhis.tae@gmail.com>
2024-03-20 22:48:02 +0700

    Add Raleway font and update blue colors in tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,15 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        blue1: '#5356FF',
+        blue2: '#378CE7',
+        blue3: '#67C6E3',
+        blue4: '#DFF5FF',
+      },
+      fontFamily: {
+        raleway: ['Raleway', 'sans-serif'],
+      },
     },
   },
   plugins: [],
