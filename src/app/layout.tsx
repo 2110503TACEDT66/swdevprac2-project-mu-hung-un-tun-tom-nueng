1937d064c2f16cc75baeae2d4f404b54b1e30d1d
MoosaTae <sadhis.tae@gmail.com>
2024-03-21 00:37:54 +0700

    add Navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css';
+import TopMenu from '@/components/TopMenu';
 
 export default function RootLayout({
   children,
@@ -7,7 +8,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <TopMenu />
+        {children}
+      </body>
     </html>
   );
 }
