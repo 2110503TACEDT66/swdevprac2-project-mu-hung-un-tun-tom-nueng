f939620575ce53aaa6a7c2ccb16474d8f77ebb74
MoosaTae <sadhis.tae@gmail.com>
2024-03-24 16:10:08 +0700

    add authen to company page and CardPanel

--- src/libs/getAllCompany.tsx
+++ src/libs/getAllCompany.tsx
@@ -1,7 +1,13 @@
-export default async function getAllCompany() {
+export default async function getAllCompany(token: string) {
   await new Promise((resolve) => setTimeout(resolve, 1000));
   const response = await fetch(
-    'https://job-fair-frontend-but-backend.vercel.app/company'
+    'https://job-fair-frontend-but-backend.vercel.app/company/',
+    {
+      method: 'GET',
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    }
   );
   if (!response.ok) {
     throw new Error('Failed to fetch all company');
