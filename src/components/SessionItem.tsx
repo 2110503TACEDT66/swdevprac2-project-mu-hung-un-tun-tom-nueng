be4a3038ce475b6266ed4b6fc0be90a9540e9df6
datswhaaan <145758336+datswhaaan@users.noreply.github.com>
2024-03-25 02:26:11 +0000

    add edit button

--- src/components/SessionItem.tsx
+++ src/components/SessionItem.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 export default function SessionItem({
   company,
   date,
@@ -11,12 +13,20 @@ export default function SessionItem({
         <div className="text-2xl font-bold">{company.name}</div>
         <div>{date.toString()}</div>
       </div>
-      <button
-        className="self-end rounded-3xl border-2 border-blue1 bg-blue1 px-8
-                        py-1 text-white hover:bg-white hover:text-blue1"
-      >
-        Edit
-      </button>
+      <div className="flex flex-row self-end">
+        <div
+          className="mx-1 rounded-3xl border-2 border-blue3 bg-blue3 px-8 
+                          py-1 text-white hover:bg-white hover:text-blue3"
+        >
+          <Link href="#">Edit</Link>
+        </div>
+        <button
+          className="rounded-3xl border-2 border-blue1 bg-blue1 px-8
+                          py-1 text-white hover:bg-white hover:text-blue1"
+        >
+          Delete
+        </button>
+      </div>
     </div>
   );
 }
